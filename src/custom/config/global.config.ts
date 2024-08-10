b074ecd450a195e9aa4612357aba612784d40c7f
edward xu <heng.xu@biogeom.com>
2024-08-10 16:09:29 +0800

    mod: api host

--- src/custom/config/global.config.ts
+++ src/custom/config/global.config.ts
@@ -1,5 +1,5 @@
-export const BACKEND_HOST = window.location.host.includes(
-    'https://geobiologics-cn.biogeom.com'
+export const BACKEND_HOST = window.location.host.startsWith(
+    'geobiologics-cn.biogeom.com'
 )
     ? 'https://geobiologics-backend.biogeom.com'
     : 'http://geobiologics-cn-api-test.biogeom.com';
