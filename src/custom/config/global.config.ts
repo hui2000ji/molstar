e8cc7bb0df4704c1c2929192b7790bea32afc602
edward xu <heng.xu@biogeom.com>
2024-08-16 10:58:00 +0800

    fix: domain issue

--- src/custom/config/global.config.ts
+++ src/custom/config/global.config.ts
@@ -1,5 +1,7 @@
-export const BACKEND_HOST = window.location.host.startsWith(
+export const BACKEND_HOST = (window.location.host.startsWith(
     'geobiologics-cn.biogeom.com'
-)
+) || window.location.host.startsWith(
+    'geobiologics.biogeom.com'
+))
     ? 'https://geobiologics-backend.biogeom.com'
     : 'http://geobiologics-cn-api-test.biogeom.com';
